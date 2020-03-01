db38cd56f24e19642f20afb189be5241c119974f
Martin Hartl <martin@hartl.co>
2020-03-01 10:52:05 +0100

    Add basic detail view

--- OpenDay/ContentView.swift
+++ OpenDay/ContentView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import KingfisherSwiftUI
 
 struct ContentView: View {
     @FetchRequest(fetchRequest: EntryPost.allPostsFetchRequest()) var posts: FetchedResults<EntryPost>
@@ -6,12 +7,20 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                NavigationLink(destination: AddView(),
+                NavigationLink(destination: EntryView(),
                                label: {
                                 Text("Add")
                 })
                 List(posts) { post in
-                    Text(post.title ?? "")
+                    NavigationLink(destination: EntryView(entry: post)) {
+                        VStack(alignment: .leading) {
+                            Text(post.title ?? "")
+                                .font(.headline)
+                            Text(post.body ?? "")
+                            Text(post.entryDate?.description ?? "")
+                                .font(.footnote)
+                        }
+                    }
                 }
             }
             .navigationBarTitle("Entries")
@@ -19,21 +28,49 @@ struct ContentView: View {
     }
 }
 
-struct AddView: View {
+struct EntryView: View {
     @State var title = ""
+    @State var bodyString = ""
+    @State var images = [UIImage]()
     @Environment(\.managedObjectContext) var managedObjectContext
+
+    private var entry: EntryPost?
+
+    init() { }
+
+    init(entry: EntryPost) {
+        self.title = entry.title ?? ""
+        self.bodyString = entry.body ?? ""
+
+        self.entry = entry
+    }
 
     var body: some View {
         VStack {
-            TextField("Title", text: $title)
-            Button(action: {
-                let entry = EntryPost(context: self.managedObjectContext)
-                entry.title = self.title
-                entry.entryDate = Date()
-                try? self.managedObjectContext.save()
-            }) {
-                Text("Save")
-            }
+            List {
+                Section {
+                    TextField("Title", text: $title)
+                        .font(.headline)
+                    TextField("Content", text: $bodyString)
+                        .font(.body)
+                    Button(action: {
+
+                    }) {
+                        Text("Add Image")
+                    }
+                }
+                Section {
+                    Button(action: {
+                        let entry = EntryPost(context: self.managedObjectContext)
+                        entry.title = self.title
+                        entry.body = self.bodyString
+                        entry.entryDate = Date()
+                        try? self.managedObjectContext.save()
+                    }) {
+                        Text("Save")
+                    }
+                }
+            }.listStyle(GroupedListStyle())
         }
     }
 }
