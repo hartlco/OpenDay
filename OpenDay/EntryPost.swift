e4c338e57dc6943834339c4682daa808935c0e62
Martin Hartl <martin@hartl.co>
2020-03-14 06:12:51 +0100

    Disable automatic CoreData code generation

--- OpenDay/EntryPost.swift
+++ OpenDay/EntryPost.swift
@@ -25,12 +25,25 @@ public class EntryPost: NSManagedObject, Identifiable {
     }
 }
 
-extension EntryImage: Identifiable { }
+public class EntryImage: NSManagedObject, Identifiable {
+    @NSManaged public var data: Data?
+    @NSManaged public var imageDate: Date?
+    @NSManaged public var post: EntryPost?
+}
 
 extension EntryImage {
     var openImage: OKImage {
         return OKImage(data: data!)!
     }
+}
+
+public class EntryLocation: NSManagedObject, Identifiable {
+    @NSManaged public var city: String?
+    @NSManaged public var isoCountryCode: String?
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var street: String?
+    @NSManaged public var post: EntryPost?
 }
 
 extension EntryLocation {
