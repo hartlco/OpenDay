e622dfd701ba28e0c36d2c411741f90d0a18e9d7
Martin Hartl <martin@hartl.co>
2020-05-08 06:17:43 +0200

    Use tabView instead of custom implementation

--- OpenDay/ContentView.swift
+++ OpenDay/ContentView.swift
@@ -26,64 +26,64 @@ struct ContentView: View {
     var mapView: some View {
         return MapView(locations: $store.locations) { location in
             self.isModal = true
-//            self.selectedModalEntry = location.getPost()
+            //            self.selectedModalEntry = location.getPost()
         }.edgesIgnoringSafeArea(.vertical)
     }
 
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    ForEach(store.sections) { (section: EntriesSection) in
-                            Text(section.title)
-                                .font(Font.title.smallCaps()).bold()
-                            ForEach(section.posts) { (post: Entry) in
-                                EntryRowView(post: post) {
-                                    self.isModal = true
-                                    self.selectedModalEntry = post
-                                }
-                                .sheet(isPresented: self.$isModal, content: {
-                                    self.selectedModalEntry.map { post in
-                                        NavigationView {
-                                            EntryView().environmentObject(self.store.store(for: post))
-                                        }
-                                    }
-                                })
-                                .contextMenu {
-                                    Button(action: {
-                                        self.store.delete(entry: post)
-                                    }, label: {
-                                        Text("Delete")
-                                        Image(systemName: "trash")
-                                    })
-                                }
-                        }
-                    }
+            TabView {
+                contentList
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Entries")
                 }
-                .listStyle(DefaultListStyle())
-                HStack {
-                    Spacer()
-                    NavigationLink(destination: EntryView().environmentObject(store.storeForNewEntry()),
-                                   label: {
-                                    Image(systemName: "plus.circle.fill")
-                                        .resizable()
-                                        .frame(width: 32.0, height: 32.0, alignment: .center)
-                                        .padding()
-                    })
-                    NavigationLink(destination: mapView.sheet(isPresented: $isModal, content: {
-                        self.selectedModalEntry.map { post in
-                            EntryView().environmentObject(self.store.store(for: post))
-                        }
-                    })) {
+                mapView
+                    .tabItem {
                         Image(systemName: "map")
-                        .resizable()
-                        .frame(width: 32.0, height: 32.0, alignment: .center)
-                        .padding()
-                    }
-                    Spacer()
+                        Text("Map")
                 }
             }
-//            .navigationBarTitle("Entries")
+            .navigationBarItems(trailing: addButton)
+            .navigationBarTitle("Entries")
         }
+    }
+
+    var contentList: some View {
+        List {
+            ForEach(store.sections) { (section: EntriesSection) in
+                Text(section.title)
+                    .font(Font.title.smallCaps()).bold()
+                ForEach(section.posts) { (post: Entry) in
+                    EntryRowView(post: post) {
+                        self.isModal = true
+                        self.selectedModalEntry = post
+                    }
+                    .sheet(isPresented: self.$isModal, content: {
+                        self.selectedModalEntry.map { post in
+                            NavigationView {
+                                EntryView().environmentObject(self.store.store(for: post))
+                            }
+                        }
+                    })
+                        .contextMenu {
+                            Button(action: {
+                                self.store.delete(entry: post)
+                            }, label: {
+                                Text("Delete")
+                                Image(systemName: "trash")
+                            })
+                    }
+                }
+            }
+        }
+        .listStyle(DefaultListStyle())
+    }
+
+    var addButton: some View {
+        NavigationLink(destination: EntryView().environmentObject(store.storeForNewEntry()),
+                       label: {
+                        Image(systemName: "plus.circle.fill")
+        })
     }
 }
